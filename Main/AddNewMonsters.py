898282fa702b31db8b3e0ed8194472475b78e1ae
Nicholas Conn <188623nc@gmail.com>
2021-01-08 17:17:58 -0800

    Performance issues Resolved
    Remember to change path back.

--- Main/AddNewMonsters.py
+++ Main/AddNewMonsters.py
@@ -1,26 +1,27 @@
-import pyautogui as PAG
-import win32api as Win
+import pyautogui as pag
+import win32api as win
 from os import path
 
-NoClick = Win.GetKeyState(0x01)
+NoClick = win.GetKeyState(0x01)
 while True:
-    a = Win.GetKeyState(0x01)
+    a = win.GetKeyState(0x01)
 
 
-    def AddMonster():
-        x, y = PAG.position()
-        MonsterLogo = PAG.screenshot(region=(x - 6, y - 6, 12, 12))
-        SaveFile = str(input("Enter the monster name:\n"))
-        FileName = 'C:\\Users\\18862\\Desktop\\Overworld Bot\\Main\\ScaledMonsters' + '\\' + SaveFile + '.png'
-        Count = 0
-        while path.exists(FileName):
-            Count += 1
-            FileName = 'C:\\Users\\18862\\Desktop\\Overworld Bot\\Main\\ScaledMonsters' + '\\' \
-                       + SaveFile + str(Count) + '.png'
+    def addmonster():
+        x, y = pag.position()
+        monster_logo = pag.screenshot(region=(x - 6, y - 6, 12, 12))
+        save_file = str(input("Enter the monster name:\n"))
+        file_name = 'C:\\Users\\Nick Conn\\Documents\\GitHub\\Overworld-Bot\\Main\\ScaledMonsters' + '\\' + save_file \
+                    + '.png'
+        count = 0
+        while path.exists(file_name):
+            count += 1
+            file_name = 'C:\\Users\\Nick Conn\\Documents\\GitHub\\Overworld-Bot\\Main\\ScaledMonsters' + '\\' \
+                        + save_file + str(count) + '.png'
 
-        MonsterLogo.save(FileName)
-        print("Saved as: " + FileName)
+        monster_logo.save(file_name)
+        print("Saved as: " + file_name)
 
 
     if a != NoClick:
-        AddMonster()
+        addmonster()
--- Main/Final.py
+++ Main/Final.py
@@ -1,6 +1,6 @@
 # # Final Version Goal:
 # # Avoid image recognition for finding buttons
-# #     less automatated, more efficient
+# #     less automated, more efficient
 # # Require:
 # #   -Continue Screen(Fight Monster)
 # #   -Attack Screen
@@ -20,15 +20,15 @@
 #                         -image rec of either att button or continue(victory) button
 #                 +Click Victory/Defeat button to return to main scene
 #                 +Click Heal Button
-#                     -Potion of choicesss
+#                     -Potion of choices
 #     ---Return To beginning of farming function
 import glob
 import random
 from datetime import datetime
 
-import pyautogui as PAG
-import win32gui as Win
-import cv2 as C
+import pyautogui as pag
+import win32gui as win
+import cv2 as c
 import time
 
 StartTime = datetime.now()
@@ -38,7 +38,7 @@ CBL = (476, 606)  # Continue Button Location
 ABL = (372, 892)  # Attack Button Location
 VBL = (484, 980)  # Victory Button Location
 HBL = (582, 999)  # Heal Button Location
-PBL = (578, 818)  # Potion Button Location
+PBL = (578, 750)  # Potion Button Location
 XBL = (480, 1030)  # 'X' Button Location
 SBL = (480, 825)  # Location necessary for scroll
 EXP = (480, 500)  # EXP Potion
@@ -47,107 +47,106 @@ OCL = (560, 650)  # Occult Candle
 TL = (638, 644)  # Torch
 
 
-def CreateMonsterList(MonsterDirectory):
-    SML = []
-    path = glob.glob(MonsterDirectory)
+def create_monster_list(monster_directory):
+    sml = []
+    path = glob.glob(monster_directory)
     for img in path:
-        SML.append(img)
-    return SML
+        sml.append(img)
+    return sml
 
 
-def GetScreenshot(Selected_Window):
-    if Selected_Window:
-        window = Win.FindWindow(None, Selected_Window)
+def get_screenshot(selected_window):
+    if selected_window:
+        window = win.FindWindow(None, selected_window)
         if window:
-            Win.SetForegroundWindow(window)
-            x, y, x1, y1 = Win.GetClientRect(window)
-            x, y = Win.ClientToScreen(window, (x + 240, y + 240))
-            x1, y1 = Win.ClientToScreen(window, ((x1 - 240) - x, (y1 - 240) - y))
-            Screenshot = PAG.screenshot("CurrentScreenshot.png", region=(x, y, x1, y1))
+            win.SetForegroundWindow(window)
+            x, y, x1, y1 = win.GetClientRect(window)
+            x, y = win.ClientToScreen(window, (x + 240, y + 240))
+            x1, y1 = win.ClientToScreen(window, ((x1 - 240) - x, (y1 - 240) - y))
+            screenshot = pag.screenshot("CurrentScreenshot.png", region=(x, y, x1, y1))
             print("updated 'CurrentScreenshot.png'")
-            return Screenshot
+            return screenshot
     else:
         print("invalid Source")
         return None
 
 
-def FoundMonster(Monster):
-    print("Looking for " + Monster)
-    GetScreenshot('Pixel 5')
-    MI = C.imread('CurrentScreenshot.png')
-    GMI = C.cvtColor(MI, C.COLOR_BGR2GRAY)
-    S = C.imread(Monster, 0)
-    width, height = S.shape[:2]
-    MatchChecker = C.matchTemplate(GMI, S, C.TM_CCOEFF_NORMED)
-    accuracy = .9
-    min_val, max_val, min_loc, max_loc = C.minMaxLoc(MatchChecker)
+def found_monster(_monster):
+    print("Looking for " + _monster)
+    get_screenshot('SM-G973U')
+    mi = c.imread('CurrentScreenshot.png')
+    gmi = c.cvtColor(mi, c.COLOR_BGR2GRAY)
+    s = c.imread(_monster, 0)
+    width, height = s.shape[:2]
+    match_checker = c.matchTemplate(gmi, s, c.TM_CCOEFF_NORMED)
+    accuracy = .8
+    min_val, max_val, min_loc, max_loc = c.minMaxLoc(match_checker)
     if max_val > accuracy:
         top_left = max_loc
         bottom_right = (top_left[0] + width, top_left[1] + height)
-        CenterCords = [0, 1]
-        CenterCords[0] = ((top_left[0] + bottom_right[0]) / 2) + 238
-        CenterCords[1] = ((top_left[1] + bottom_right[1]) / 2) + 245
-        C.rectangle(MI, top_left, bottom_right, 255, 2)
-        C.rectangle(MI, CenterCords, CenterCords, 255, 5)
+        center_cords = [0, 1]
+        center_cords[0] = ((top_left[0] + bottom_right[0]) / 2) + 238
+        center_cords[1] = ((top_left[1] + bottom_right[1]) / 2) + 245
+        c.rectangle(mi, top_left, bottom_right, 255, 2)
+        c.rectangle(mi, center_cords, center_cords, 255, 5)
 
-        if len(CenterCords) != 0:
-            print(Monster + " Was Found!")
-            PAG.moveTo(CenterCords[0] + width, CenterCords[1] + height, duration=.2)
-            Fx, Fy = PAG.position()
-            PAG.click(Fx + random.uniform(0, 1.0), Fy + random.uniform(0, 1), duration=.2)
-            C.imwrite("detected.png", MI)
+        if len(center_cords) != 0:
+            print(_monster + " Was Found!")
+            pag.moveTo(center_cords[0] + width, center_cords[1] + height, duration=.2)
+            fx, fy = pag.position()
+            pag.click(fx + random.uniform(0, 1.0), fy + random.uniform(0, 1), duration=.2)
+            c.imwrite("detected.png", mi)
             return True
         else:
             return False
 
 
 while True:
-
     CurrentTime = datetime.now()
-    for Monsters in CreateMonsterList(r"C:\Users\18862\Desktop\Overworld Bot\Main\ScaledMonsters\*.png"):
-        if FoundMonster(Monsters):
+    for Monsters in create_monster_list(r"C:\Users\Nick Conn\Documents\GitHub\Overworld-Bot\Main\ScaledMonsters\*.png"):
+        if found_monster(Monsters):
             # Hit Continue Button
-            PAG.moveTo(CBL[0], CBL[1], duration=.6)
-            PAG.click(CBL[0] + random.uniform(1.0, 10.0), CBL[1] + random.uniform(.01, 1), duration=.1)
+            pag.moveTo(CBL[0], CBL[1], duration=.6)
+            pag.click(CBL[0] + random.uniform(1.0, 10.0), CBL[1] + random.uniform(.01, 1), duration=.1)
             print("Clicked Continue")
             # Hit Attack Button
-            PAG.click(ABL[0] + random.uniform(10, 30), ABL[1] + random.uniform(1.0, 5.0), duration=.8)
+            pag.click(ABL[0] + random.uniform(10, 30), ABL[1] + random.uniform(1.0, 5.0), duration=.8)
             print("Clicked Attack 1")
             # Hit Attack Button
-            PAG.click(ABL[0] + random.uniform(3.0, 10.0), ABL[1] + random.uniform(1.0, 5.0), duration=4)
+            pag.click(ABL[0] + random.uniform(3.0, 10.0), ABL[1] + random.uniform(1.0, 5.0), duration=4)
             print("Clicked Attack 2")
             # Hit Victory Button
-            PAG.click(VBL[0] + random.uniform(1.0, 20), VBL[1] - 15, duration=3)
+            pag.click(VBL[0] + random.uniform(1.0, 20), VBL[1] - 15, duration=3)
             print("Clicked Continue")
             # Hit Heal Button
-            PAG.click(HBL[0] + random.uniform(1.0, 8.0), HBL[1] + random.uniform(1.0, 8.0), duration=.8)
+            pag.click(HBL[0] + random.uniform(1.0, 8.0), HBL[1] + random.uniform(1.0, 8.0), duration=.8)
             print("Clicked Heal Menu")
             # Hit Mana Button
-            PAG.click(PBL[0] + random.uniform(1.0, 5.0), PBL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(PBL[0] + random.uniform(1.0, 5.0), PBL[1] + random.uniform(1.0, 1.3), duration=1.2)
             print("Clicked Potion")
             # Hit 'x' Button
-            PAG.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
             print("Clicked X")
             time.sleep(2)
 
 
-        def XPBuff():
+        def xp_buff():
             print("Timer went off")
-            PAG.click(HBL[0] + random.uniform(1.0, 5.0), HBL[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.moveTo(SBL[0] + random.uniform(1.0, 5.0), SBL[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.vscroll(-800)
+            pag.click(HBL[0] + random.uniform(1.0, 5.0), HBL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.moveTo(SBL[0] + random.uniform(1.0, 5.0), SBL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.vscroll(-800)
 
             print("Done scroll?")
-            PAG.click(EXP[0] + random.uniform(1.0, 5.0), EXP[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.click(LCL[0] + random.uniform(1.0, 5.0), LCL[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.click(OCL[0] + random.uniform(1.0, 5.0), OCL[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.click(TL[0] + random.uniform(1.0, 5.0), TL[1] + random.uniform(1.0, 5.0), duration=1)
-            PAG.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(EXP[0] + random.uniform(1.0, 5.0), EXP[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(LCL[0] + random.uniform(1.0, 5.0), LCL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(OCL[0] + random.uniform(1.0, 5.0), OCL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(TL[0] + random.uniform(1.0, 5.0), TL[1] + random.uniform(1.0, 5.0), duration=1)
+            pag.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
 
 
         if CurrentTime.hour == StartTime.hour + 1:
             print("HOUR SAME")
             if CurrentTime.minute == StartTime.minute:
                 print("-----------------------REBUFFING-----------------")
-                XPBuff()
+                xp_buff()
                 StartTime = datetime.now()
