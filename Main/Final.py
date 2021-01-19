32def0bb839619d715840b61858b979c539e9e6b
Nicholas Conn <188623nc@gmail.com>
2021-01-18 23:50:37 -0800

    More Monster  + performance
    yep

--- Main/Final.py
+++ Main/Final.py
@@ -31,20 +31,64 @@ import win32gui as win
 import cv2 as c
 import time
 
-StartTime = datetime.now()
-print("Program has started running at: " + str(StartTime))
+
+class Colors:
+    HEADER = '\033[95m'
+    OK_BLUE = '\033[94m'
+    OK_CYAN = '\033[96m'
+    OK_GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END_C = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 CBL = (476, 606)  # Continue Button Location
 ABL = (372, 892)  # Attack Button Location
+DBL = (600, 882)  # DAttack Button Location
 VBL = (484, 980)  # Victory Button Location
 HBL = (582, 999)  # Heal Button Location
-PBL = (578, 750)  # Potion Button Location
-XBL = (480, 1030)  # 'X' Button Location
+PBL = (641, 586)  # Potion Button Location
+XBL = (480, 1025)  # 'X' Button Location
 SBL = (480, 825)  # Location necessary for scroll
-EXP = (480, 500)  # EXP Potion
-LCL = (314, 640)  # Lucky Coin
-OCL = (560, 650)  # Occult Candle
-TL = (638, 644)  # Torch
+EXP = (480, 782)  # EXP Potion
+LCL = (640, 775)  # Lucky Coin
+SCL = (322, 916)  # silver Coin
+OCL = (528, 926)  # Occult Candle
+ACL = (314, 742)  # A Candle
+TL = (629, 916)  # Torch
+
+
+def xp_buff():
+    print(Colors.FAIL + "--REBUFFING--" + Colors.END_C)
+
+    # print("Timer went off")
+    pag.click(HBL[0] + random.uniform(1.0, 5.0), HBL[1] + random.uniform(1.0, 5.0), duration=1)
+    # pag.moveTo(SBL[0] + random.uniform(1.0, 5.0), SBL[1] + random.uniform(1.0, 5.0), duration=1)
+    # pag.vscroll(-800)
+    #
+    # print("Done scroll?")
+    pag.click(EXP[0] + random.uniform(1.0, 5.0), EXP[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(LCL[0] + random.uniform(1.0, 5.0), LCL[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(SCL[0] + random.uniform(1.0, 5.0), SCL[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(OCL[0] + random.uniform(1.0, 5.0), OCL[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(ACL[0] + random.uniform(1.0, 5.0), ACL[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(TL[0] + random.uniform(1.0, 5.0), TL[1] + random.uniform(1.0, 5.0), duration=1)
+    pag.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
+
+
+open('MonsterLog.txt', 'w').close()
+file = open("MonsterLog.txt", "w+")
+
+StartTime = datetime.now()
+Buff_time = StartTime.hour + 1
+
+
+# Ask_Rebuff = str(input("Rebuff? Y/N\n"))
+#
+# if Ask_Rebuff == 'Y':
+#     xp_buff()
 
 
 def create_monster_list(monster_directory):
@@ -64,7 +108,6 @@ def get_screenshot(selected_window):
             x, y = win.ClientToScreen(window, (x + 240, y + 240))
             x1, y1 = win.ClientToScreen(window, ((x1 - 240) - x, (y1 - 240) - y))
             screenshot = pag.screenshot("CurrentScreenshot.png", region=(x, y, x1, y1))
-            print("updated 'CurrentScreenshot.png'")
             return screenshot
     else:
         print("invalid Source")
@@ -72,14 +115,18 @@ def get_screenshot(selected_window):
 
 
 def found_monster(_monster):
-    print("Looking for " + _monster)
+    ShortString = str(_monster).rsplit("\\", 1)
+    No_PNG = ShortString[1].rsplit('.', 1)
+    No_Num = ''.join([i for i in No_PNG[0] if not i.isdigit()])
+    print(Colors.WARNING + "Looking for " + str(No_Num) + Colors.END_C)
+
     get_screenshot('SM-G973U')
     mi = c.imread('CurrentScreenshot.png')
     gmi = c.cvtColor(mi, c.COLOR_BGR2GRAY)
     s = c.imread(_monster, 0)
     width, height = s.shape[:2]
     match_checker = c.matchTemplate(gmi, s, c.TM_CCOEFF_NORMED)
-    accuracy = .8
+    accuracy = .85
     min_val, max_val, min_loc, max_loc = c.minMaxLoc(match_checker)
     if max_val > accuracy:
         top_left = max_loc
@@ -91,11 +138,12 @@ def found_monster(_monster):
         c.rectangle(mi, center_cords, center_cords, 255, 5)
 
         if len(center_cords) != 0:
-            print(_monster + " Was Found!")
+            print(Colors.OK_GREEN + No_Num + " Was Found!" + Colors.END_C)
             pag.moveTo(center_cords[0] + width, center_cords[1] + height, duration=.2)
             fx, fy = pag.position()
             pag.click(fx + random.uniform(0, 1.0), fy + random.uniform(0, 1), duration=.2)
             c.imwrite("detected.png", mi)
+            file.write(No_Num + "\n")
             return True
         else:
             return False
@@ -108,45 +156,26 @@ while True:
             # Hit Continue Button
             pag.moveTo(CBL[0], CBL[1], duration=.6)
             pag.click(CBL[0] + random.uniform(1.0, 10.0), CBL[1] + random.uniform(.01, 1), duration=.1)
-            print("Clicked Continue")
             # Hit Attack Button
             pag.click(ABL[0] + random.uniform(10, 30), ABL[1] + random.uniform(1.0, 5.0), duration=.8)
-            print("Clicked Attack 1")
             # Hit Attack Button
-            pag.click(ABL[0] + random.uniform(3.0, 10.0), ABL[1] + random.uniform(1.0, 5.0), duration=4)
-            print("Clicked Attack 2")
+            pag.click(ABL[0] + random.uniform(3.0, 10.0), ABL[1] + random.uniform(1.0, 5.0), duration=5)
+            pag.click(DBL[0] + random.uniform(10, 30), DBL[1] + random.uniform(1.0, 5.0), duration=1.2)
             # Hit Victory Button
             pag.click(VBL[0] + random.uniform(1.0, 20), VBL[1] - 15, duration=3)
-            print("Clicked Continue")
             # Hit Heal Button
             pag.click(HBL[0] + random.uniform(1.0, 8.0), HBL[1] + random.uniform(1.0, 8.0), duration=.8)
-            print("Clicked Heal Menu")
             # Hit Mana Button
             pag.click(PBL[0] + random.uniform(1.0, 5.0), PBL[1] + random.uniform(1.0, 1.3), duration=1.2)
-            print("Clicked Potion")
             # Hit 'x' Button
             pag.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
-            print("Clicked X")
             time.sleep(2)
 
-
-        def xp_buff():
-            print("Timer went off")
-            pag.click(HBL[0] + random.uniform(1.0, 5.0), HBL[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.moveTo(SBL[0] + random.uniform(1.0, 5.0), SBL[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.vscroll(-800)
-
-            print("Done scroll?")
-            pag.click(EXP[0] + random.uniform(1.0, 5.0), EXP[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.click(LCL[0] + random.uniform(1.0, 5.0), LCL[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.click(OCL[0] + random.uniform(1.0, 5.0), OCL[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.click(TL[0] + random.uniform(1.0, 5.0), TL[1] + random.uniform(1.0, 5.0), duration=1)
-            pag.click(XBL[0] + random.uniform(1.0, 5.0), XBL[1] + random.uniform(1.0, 5.0), duration=1)
-
-
-        if CurrentTime.hour == StartTime.hour + 1:
-            print("HOUR SAME")
+        if CurrentTime.hour == Buff_time:
             if CurrentTime.minute == StartTime.minute:
-                print("-----------------------REBUFFING-----------------")
+                file.write("REBUFFING")
                 xp_buff()
+                print("StartTime Reset: " + str(StartTime))
+                print("CurrentTime Reset: " + str(CurrentTime))
                 StartTime = datetime.now()
+                Buff_time = StartTime.hour + 1
