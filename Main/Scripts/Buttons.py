f135203b8df5b3a53bdf063ef772ddc3ba60cd30
Nicholas Conn <188623nc@gmail.com>
2021-01-21 00:24:57 -0800

    Update Buttons.py
    Updated locations for newer layout,
    need to implement automated location check and place

--- Main/Scripts/Buttons.py
+++ Main/Scripts/Buttons.py
@@ -1,7 +1,7 @@
 class Buttons:
     # Button locations in relation to screen location
     # implement automation to allow for increased compat.
-    CBL = (476, 606)  # Continue Button Location
+    CBL = (476, 615)  # Continue Button Location
     ABL = (372, 892)  # Attack Button Location
     DBL = (600, 882)  # DAttack Button Location
     VBL = (484, 980)  # Victory Button Location
@@ -9,9 +9,12 @@ class Buttons:
     PBL = (641, 586)  # Potion Button Location
     XBL = (480, 1025)  # 'X' Button Location
     SBL = (480, 825)  # Location necessary for scroll
-    EXP = (480, 782)  # EXP Potion
-    LCL = (640, 775)  # Lucky Coin
-    SCL = (322, 916)  # silver Coin
-    OCL = (528, 926)  # Occult Candle
-    ACL = (314, 742)  # A Candle
-    TL = (629, 916)  # Torch
+    EXP = (480, 861)  # EXP Potion
+    LCL = (554, 853)  # Lucky Coin
+    SCL = (640, 851)  # silver Coin
+    OCL = (379, 968)  # Occult Candle
+    ACL = (320, 850)  # A Candle
+    TL = (583, 966)  # Torch
+
+
+#CHECK LOCATION BASED ON YOUR PHONE
